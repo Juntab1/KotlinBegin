22c6ec65d7c4d6a53ee65747dfdf108412ac0bbf
juntab1 <jsbwhaleguy@gmail.com>
2023-12-02 16:54:38 -0800

    String manipulation, if statements, and when statements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 // top level variable, non-null by default, can't make it equal to null
 var name : String = "Jun1"
+
 // if you want it to be maybe null, use the ?
 val name2: String? = "Jun4"
+
 // don't have to say the type but if you do it can't be null
 var noType = 1
 
@@ -17,14 +19,16 @@ fun main() {
 
     // mutable vairables: can be changed
     var changable: String = "Jun2"
+
     // unmutable
     val notChange: String = "Jun3"
+
 //    check if it is null
 //    if (name2 != null){
 //        println(name2)
 //    }
 
-    // used for checking a variable and see if it needs to be switched on
+//    used for checking a variable and see if it needs to be switched on
 //    when(name2){
 //        null -> println("it's null")
 //        else -> println(name2)

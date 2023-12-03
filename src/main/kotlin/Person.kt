579f05edc4714c1161c36003191fe6ce95a7d424
juntab1 <jsbwhaleguy@gmail.com>
2023-12-02 19:45:14 -0800

    classes and playing around with constructor

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -4,4 +4,15 @@ class Person(val firstName: String, val lastName: String) {
 //    val firstName: String = _firstName
 //    val lastName: String = _lastName
 
+    // init 1 and 2 are always run through but the constructor depends on if parameters are passed in for the class or not
+    init {
+        println("init 1")
+    }
+    constructor(): this("Jun", "B") {
+        println("secondary constructor")
+    }
+
+    init {
+        println("init 2")
+    }
 }

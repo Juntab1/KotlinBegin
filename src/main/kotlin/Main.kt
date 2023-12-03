f329181a15b7eea766d7a435d0f763ea71554f03
juntab1 <jsbwhaleguy@gmail.com>
2023-12-02 17:19:51 -0800

    for each variant and array usage

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,23 @@ fun sayHello(name:String){
     println("Hello $name")
 }
 
-// single statement version
-//fun sayHello(name:String) = println("Hello $name")
-
-
 
 fun main() {
-    sayHello("Jun")
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    println(interestingThings.size)
+    println(interestingThings[1])
+    println(interestingThings.get(2))
+
+    // Usually use it because that is the standard but can change it with "whatever ->"
+    interestingThings.forEach { interestingThing ->
+        println(interestingThing)
+    }
+
+    interestingThings.forEachIndexed { index, interestingThing ->
+        println("$interestingThing is at index $index")
+    }
+
 }
+
 
 

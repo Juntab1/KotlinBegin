071f4b7c16ffd9cf00aad51d99ffd873b497be2f
juntab1 <jsbwhaleguy@gmail.com>
2023-12-02 17:36:59 -0800

    pass in collection type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,31 @@
-fun sayHello(name:String){
-    println("Hello $name")
+fun sayHello(itemToGreet:List<String>){
+    itemToGreet.forEach {
+        println(it)
+    }
 }
 
 
+
 fun main() {
-    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
-    println(interestingThings.size)
-    println(interestingThings[1])
-    println(interestingThings.get(2))
+    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
 
-    // Usually use it because that is the standard but can change it with "whatever ->"
-    interestingThings.forEach { interestingThing ->
-        println(interestingThing)
-    }
+    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach { key, value -> println("$key -> $value") }
 
-    interestingThings.forEachIndexed { index, interestingThing ->
-        println("$interestingThing is at index $index")
-    }
+    // by default a collection in Kotlin is immutable
+    // create mutable
+    val mutable = mutableListOf("3")
+    mutable.add("2")
+    println(mutable)
+    sayHello(interestingThings)
+
+//    using list
+//    val interestingThings = listOf("Kotlin", "Programming", "Comic Books")
+//    interestingThings[0]
+//    interestingThings.get(1)
+//    interestingThings.forEach {
+//        println(it)
+//    }
 
 }
 

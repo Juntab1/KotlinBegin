e5d78893ec501d104c308f2aaf3ca522de959f1c
Juntaro Bruttomesso <jsbwhaleguy@gmail.com>
2023-12-13 21:53:40 -0800

    more interface practice

--- src/main/kotlin/PersonInfoProvider.kt
+++ src/main/kotlin/PersonInfoProvider.kt
@@ -7,16 +7,24 @@ interface PersonInfoProvider {
     }
 }
 
+interface SessionInfoProvider {
+    fun getSessionId() : String
+}
+
 
 // : what you want to implement
 // abstract class is used when you don't want to use everything from the implementor
-class BasicInfoProvider : PersonInfoProvider {
+class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider {
     override val providerInfo: String
         get() = "BasicInfoProvider"
 
     override fun printInfo(person: Person) {
         super.printInfo(person)
         println("additional print stuff")
+    }
+
+    override fun getSessionId(): String {
+        return "Session"
     }
 }
 

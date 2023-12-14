b8a587840c7a23f673b65f90487f5b8fa1af7c5f
Juntaro Bruttomesso <jsbwhaleguy@gmail.com>
2023-12-13 22:54:34 -0800

    Sealed class and how it works with Enums and such

--- src/main/kotlin/EntityFactory.kt
+++ src/main/kotlin/EntityFactory.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 // Enum class, set predefined constants
 enum class EntityType {
-    EASY, MEDIUM, HARD;
+    HELP, EASY, MEDIUM, HARD;
 
     fun getFromattedName() = name.lowercase()
 }
@@ -15,23 +15,45 @@ object EntityFactory {
             EntityType.EASY -> type.name
             EntityType.MEDIUM -> type.getFromattedName()
             EntityType.HARD -> "Hard"
+            EntityType.HELP -> type.getFromattedName()
         }
-        return Entity(id, name)
+        return when(type) {
+            EntityType.EASY -> Entity.Easy(id, name)
+            EntityType.MEDIUM -> Entity.Medium(id, name)
+            EntityType.HARD -> Entity.Hard(id, name, 2f)
+            EntityType.HELP -> Entity.Help
+        }
     }
 }
 
-
-class Entity(val id: String, val name: String) {
-    override fun toString(): String {
-        return "id:$id name:$name"
+// sealed classes help us define class hierarchies, define set number of classes that
+// extend the base site
+// can have different types and properties
+// have different types of classes within it
+// also object declaration
+sealed class Entity() {
+    object Help : Entity(){
+        val name = "Help"
     }
+
+    data class Easy(val id: String, val name: String): Entity()
+    data class Medium(val id: String, val name: String): Entity()
+    data class Hard(val id: String, val name: String, val multiplier: Float): Entity()
 }
+
 
 
 fun main() {
-    val entity = EntityFactory.create(EntityType.EASY)
-    println(entity)
+    val entity:Entity = EntityFactory.create(EntityType.EASY)
+    val msg = when (entity) {
+        // singleton so no reason to have is
+        Entity.Help -> "help class"
+        is Entity.Easy -> "easy class"
+        is Entity.Hard -> "medium class"
+        is Entity.Medium -> "hard class"
+    }
 
-    val mediumEntity = EntityFactory.create(EntityType.MEDIUM)
-    println(mediumEntity)
+    println(msg)
+
+
 }
